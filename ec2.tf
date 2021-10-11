cda41bb6abe9ec447107990861dd810beda45f00
Raj <rajsaute@gmail.com>
2021-10-11 22:53:19 +0530

    null resource add for provisigning and resource iinfooutput

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 // creation of ec2 instances
-resource "aws_instance" "ec2-tf" {
+resource "aws_instance" "ec2-web-tf" {
   ami                         = data.aws_ami.ubuntu.id
   associate_public_ip_address = true
   instance_type               = var.ec2-instance-type
@@ -10,19 +10,32 @@ resource "aws_instance" "ec2-tf" {
   tags = {
     "Name" = "webserver"
   }
+/*
+  depends_on = [
+    aws_db_instance.rds-tf
+  ]
+*/
+}
+
+// null resource and provisioner is add to run the script at the time of creating the resources
+
+resource "null_resource" "null-provisionining" {
+  
 
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("C:/Users/raj/terraform.pem") // provide the ssh key path
-    host        = self.public_ip
+    private_key = file("C:/Users/raj/terraform.pem") // provide the ssh key path by creating from AWS Console
+    host        = aws_instance.ec2-web-tf.public_ip
   }
   provisioner "remote-exec" {
-    inline = ["sudo apt update", "sudo apt install apache2 -y"]
+    inline = [
+        "sudo apt update", 
+        "sudo apt install apache2 -y",
+        "sudo apt install php libapache2-mod-php php-mysql php-cli -y",
+        "echo '<?php phpinfo(); ?>'| sudo tee /var/www/html/info.php"
+        ]
 
   }
-  depends_on = [
-    aws_db_instance.rds-tf
-  ]
 }
 
