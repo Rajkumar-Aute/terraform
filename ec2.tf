183c2f1b018a7b46b2471302f22fe6490d62cd55
Raj <rajsaute@gmail.com>
2021-10-11 22:17:43 +0530

    added ec2 with apache2 dynamic installation

--- ec2.tf
+++ ec2.tf
@@ -10,4 +10,19 @@ resource "aws_instance" "ec2-tf" {
   tags = {
     "Name" = "webserver"
   }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("C:/Users/raj/terraform.pem") // provide the ssh key path
+    host        = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install apache2 -y"]
+
+  }
+  depends_on = [
+    aws_db_instance.rds-tf
+  ]
 }
+
